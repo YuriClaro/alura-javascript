b36ccd50da50e2a31ea9569b39ee370f2592fb04
Yuri Claro <yuri.claro@outlook.com>
2023-08-02 18:10:25 -0300

    Objetos em objetos

---
+++ js-objetos/aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["11555555550", "11444444440"],
+};
+
+cliente.endereço = {
+    rua: "R. Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+};
+
+console.log(cliente.endereço);
