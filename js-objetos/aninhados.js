05a0353491a3f0756531db3d9a78f533c9341201
Yuri Claro <yuri.claro@outlook.com>
2023-08-02 18:37:27 -0300

    Lista de Objetos

--- js-objetos/aninhados.js
+++ js-objetos/aninhados.js
@@ -1,3 +1,4 @@
+// Objeto cliente
 const cliente = {
     nome: "João",
     idade: 24,
@@ -5,11 +6,28 @@ const cliente = {
     telefone: ["11555555550", "11444444440"],
 };
 
-cliente.endereço = {
+// Objeto dentro de outro objeto como endereço do cliente, podendo ser mais de um
+cliente.enderecos = [{
     rua: "R. Joseph Climber",
     numero: 1337,
     apartamento: true,
     complemento: "ap 934",
-};
+}];
 
-console.log(cliente.endereço);
+// adicionando um novo endereço ao cliente
+cliente.enderecos.push({
+    rua: "R. Joseph Ladder",
+    numero: 404,
+    apartamento: false,
+});
+
+// Vai imprimir todos os endereços.
+//console.log(cliente.enderecos);
+
+// retornando apenas os apartamentos como true, utilizando o filter e uma arrow function
+const listaApenasApartamentos = cliente.enderecos.filter(
+    (enderecos) => enderecos.apartamento === true
+);
+
+// imprimindo apenas os apartamentos === true
+console.log(listaApenasApartamentos);
