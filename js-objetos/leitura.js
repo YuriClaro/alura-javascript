e5abeeaef6f2f3166cc9dfb2c2734a4f02ad8b91
Yuri Claro <yuri.claro@outlook.com>
2023-08-04 09:37:36 -0300

    Lendo um arquivo JSON

---
+++ js-objetos/leitura.js
@@ -0,0 +1,12 @@
+// J Java
+// S Script
+// O Object
+// N Notation
+
+// O require, requisita os dados de um outro arquivo .json 
+const dados = require("./cliente.json");
+
+
+// imprime como objeto, os dados obtidos pelo .json
+console.log(dados);
+console.log(typeof dados);
