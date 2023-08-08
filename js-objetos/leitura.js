0b59a0342934326d8a8e315d8b226fc4d6fbb948
Yuri Claro <yuri.claro@outlook.com>
2023-08-08 20:55:36 -0300

    Encontrando um objeto

--- js-objetos/leitura.js
+++ js-objetos/leitura.js
@@ -6,7 +6,10 @@
 // O require, requisita os dados de um outro arquivo .json 
 const dados = require("./cliente.json");
 
+// Stringificação
+const clienteEmString = JSON.stringify(dados);
 
 // imprime como objeto, os dados obtidos pelo .json
 console.log(dados);
 console.log(typeof dados);
+console.log(clienteEmString);
