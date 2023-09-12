042873e7e8022d6e7a0c33e811d8dfa6cc30fbe9
Yuri Claro <yuri.claro@outlook.com>
2023-09-12 20:37:47 -0300

    Promessas

--- js-node/index.js
+++ js-node/index.js
@@ -5,18 +5,27 @@ import { text } from 'stream/consumers';
 console.log(chalk.blue('olá mundo'));
 
 function trataErro(erro) {
-    throw new Error(erro);
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
 function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro) {
-            trataErro(erro);
-        }       
-        console.log(chalk.green(texto))
-    })
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch(trataErro)
 }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro);
+//         }       
+//         console.log(chalk.green(texto))
+//     })
+// }
 
 pegaArquivo('./arquivos/texto.md');
 
